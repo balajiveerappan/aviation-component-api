32252502a59dc8b08796d02149219ab5f5744723
Bansal <amit.bansal@capgemini.com>
2016-11-07 19:41:45 +0530

    updated

--- src/main/java/com/aviation/poc/service/impl/AviationComponentServiceImpl.java
+++ src/main/java/com/aviation/poc/service/impl/AviationComponentServiceImpl.java
@@ -1,5 +1,8 @@
 package com.aviation.poc.service.impl;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.aviation.poc.service.AviationComponentService;
@@ -7,4 +10,30 @@ import com.aviation.poc.service.AviationComponentService;
 @Service
 public class AviationComponentServiceImpl implements AviationComponentService {
 
+	@Override
+	public List<Object> getRemovedComponents(Date startDate, Date endDate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Object> getRemovedComponentsMFG(Date startDate, Date endDate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Object> getRemovedComponentsCPNSerial(Date startDate, Date endDate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Object> getRemovedComponentsTailNoOfRemoval(Date startDate, Date endDate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	
 }
