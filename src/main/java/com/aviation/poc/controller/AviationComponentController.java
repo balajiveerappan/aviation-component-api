692a004506774553b9a937de5ea2100f44887719
Bansal <amit.bansal@capgemini.com>
2016-11-07 19:31:39 +0530

    updated

--- src/main/java/com/aviation/poc/controller/AviationComponentController.java
+++ src/main/java/com/aviation/poc/controller/AviationComponentController.java
@@ -1,5 +1,88 @@
 package com.aviation.poc.controller;
 
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.aviation.poc.service.AviationComponentService;
+
+@Controller
 public class AviationComponentController {
 
+	@Autowired
+	private AviationComponentService aviationComponentService;
+		
+	@RequestMapping(value = "/splashScreen", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Object> showSplashScreen() {
+		String pattern = "yyyy-MM-dd";
+		Date sDate=null;
+		Date eDate=null;
+		try {
+			sDate =  new SimpleDateFormat(pattern).parse("2014-08-10");
+			 eDate =  new SimpleDateFormat(pattern).parse("2016-08-10");
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		List<Object> componentRemovalRept =  aviationComponentService.getRemovedComponents(sDate, eDate);
+		return componentRemovalRept;
+	}
+	
+	@RequestMapping(value = "/splashScreenMFG", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Object> showSplashScreenCPN() {
+	
+		String pattern = "yyyy-MM-dd";
+		Date sDate=null;
+		Date eDate=null;
+		try {
+			sDate =  new SimpleDateFormat(pattern).parse("2014-08-10");
+			 eDate =  new SimpleDateFormat(pattern).parse("2016-08-10");
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		 
+		List<Object> componentRemovalRept =  aviationComponentService.getRemovedComponentsMFG(sDate, eDate);
+		return componentRemovalRept;
+	}
+	
+	
+	@RequestMapping(value = "/splashScreenCPNSerial", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Object> splashScreenCPNSerial(/*@RequestBody   List<Long> componentIds*/) {
+		String pattern = "yyyy-MM-dd";
+		Date sDate=null;
+		Date eDate=null;
+		try {
+			sDate =  new SimpleDateFormat(pattern).parse("2014-08-10");
+			 eDate =  new SimpleDateFormat(pattern).parse("2016-08-10");
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		 
+		List<Object> componentRemovalRept =  aviationComponentService.getRemovedComponentsCPNSerial(sDate, eDate);
+		return componentRemovalRept;
+	}
+	
+
+	@RequestMapping(value = "/splashScreenTail", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Object> splashScreenTail() {
+		String pattern = "yyyy-MM-dd";
+		Date sDate=null;
+		Date eDate=null;
+		try {
+			sDate =  new SimpleDateFormat(pattern).parse("2014-08-10");
+			 eDate =  new SimpleDateFormat(pattern).parse("2016-08-10");
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		List<Object> componentRemovalRept =  aviationComponentService.getRemovedComponentsTailNoOfRemoval(sDate, eDate);
+		return componentRemovalRept;
+		
+	}
 }
--- src/main/java/com/aviation/poc/service/AviationComponentService.java
+++ src/main/java/com/aviation/poc/service/AviationComponentService.java
@@ -1,5 +1,12 @@
 package com.aviation.poc.service;
 
+import java.util.Date;
+import java.util.List;
+
 public interface AviationComponentService {
 
+	List<Object> getRemovedComponents(Date startDate, Date endDate);
+	List<Object> getRemovedComponentsMFG(Date startDate, Date endDate);
+	List<Object> getRemovedComponentsCPNSerial(Date startDate, Date endDate);
+	List<Object> getRemovedComponentsTailNoOfRemoval(Date startDate, Date endDate);
 }
