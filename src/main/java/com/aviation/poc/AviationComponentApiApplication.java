09f46cc851ab41dcaa98fae6a7cd9e036414b26f
Bansal <amit.bansal@capgemini.com>
2016-11-07 19:04:02 +0530

    First Commit

---
+++ src/main/java/com/aviation/poc/AviationComponentApiApplication.java
@@ -0,0 +1,12 @@
+package com.aviation.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AviationComponentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AviationComponentApiApplication.class, args);
+	}
+}
