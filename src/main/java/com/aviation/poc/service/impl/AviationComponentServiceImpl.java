9e17e6d6b9ee954313a0621b1923d61c879ed9be
Bansal <amit.bansal@capgemini.com>
2016-11-07 19:15:09 +0530

    updated

--- src/main/java/com/aviation/poc/service/impl/AviationComponentServiceImpl.java
+++ src/main/java/com/aviation/poc/service/impl/AviationComponentServiceImpl.java
@@ -1,7 +1,10 @@
 package com.aviation.poc.service.impl;
 
+import org.springframework.stereotype.Service;
+
 import com.aviation.poc.service.AviationComponentService;
 
+@Service
 public class AviationComponentServiceImpl implements AviationComponentService {
 
 }
