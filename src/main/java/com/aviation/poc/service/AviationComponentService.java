56b8b5e0e071ad790865971a30d3cd70c11cdf0b
Bansal <amit.bansal@capgemini.com>
2016-11-07 19:34:12 +0530

    updated

--- src/main/java/com/aviation/poc/service/AviationComponentService.java
+++ src/main/java/com/aviation/poc/service/AviationComponentService.java
@@ -5,8 +5,8 @@ import java.util.List;
 
 public interface AviationComponentService {
 
-	List<Object> getRemovedComponents(Date startDate, Date endDate);
-	List<Object> getRemovedComponentsMFG(Date startDate, Date endDate);
-	List<Object> getRemovedComponentsCPNSerial(Date startDate, Date endDate);
-	List<Object> getRemovedComponentsTailNoOfRemoval(Date startDate, Date endDate);
+	List<Object> getRemovedComponents(final Date startDate, final Date endDate);
+	List<Object> getRemovedComponentsMFG(final Date startDate, final Date endDate);
+	List<Object> getRemovedComponentsCPNSerial(final Date startDate, final Date endDate);
+	List<Object> getRemovedComponentsTailNoOfRemoval(final Date startDate, final Date endDate);
 }
